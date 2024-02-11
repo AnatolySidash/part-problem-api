d52c328033d266e5978d70ad038dfd473dd2089d
Anatoly SIdash <sidash85@yandex.ru>
2024-02-11 23:03:50 +0300

    fix: update get request

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors({
     origin: ['http://localhost:3001'],
     credentials: true,
     maxAge: 30,
-  }));
+}));
 
 app.get("/", (req, res) => {
     res.json('Backend is running on 3000 port!')
@@ -25,7 +25,17 @@ app.get("/", (req, res) => {
 app.get("/problems", (req, res) => {
     const query = "SELECT * FROM problems";
     db.query(query, (err, data) => {
-        if(err) return res.json(err);
+        if (err) return res.json(err);
+        return res.json(data);
+    });
+});
+
+app.get("/problems/:id", (req, res) => {
+    const problemId = req.params.id;
+    const query = "SELECT * FROM problems WHERE id = ?";
+
+    db.query(query, [problemId], (err, data) => {
+        if (err) return res.json(err);
         return res.json(data);
     });
 });
@@ -33,22 +43,22 @@ app.get("/problems", (req, res) => {
 app.post("/problems", (req, res) => {
     const query = "INSERT INTO problems (`problem_code`, `problem`, `defect_qty`, `supplier`, `temp_cm`, `temp_cm_date`, `perm_cm`, `perm_cm_date`, `status`, `responsible`, `system`, `comment`) VALUES (?)";
     const values = [
-        req.body.problem_code, 
-        req.body.problem, 
-        req.body.defect_qty, 
-        req.body.supplier, 
-        req.body.temp_cm, 
-        req.body.temp_cm_date, 
-        req.body.perm_cm, 
-        req.body.perm_cm_date, 
-        req.body.status, 
-        req.body.responsible, 
-        req.body.system, 
-        req.body.comment, 
+        req.body.problem_code,
+        req.body.problem,
+        req.body.defect_qty,
+        req.body.supplier,
+        req.body.temp_cm,
+        req.body.temp_cm_date,
+        req.body.perm_cm,
+        req.body.perm_cm_date,
+        req.body.status,
+        req.body.responsible,
+        req.body.system,
+        req.body.comment,
     ];
 
     db.query(query, [values], (err, data) => {
-        if(err) return res.json(err);
+        if (err) return res.json(err);
         return res.json(data);
     });
 });
